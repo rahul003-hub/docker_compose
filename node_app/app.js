faed3bd47349e5e254aed5c49714a0102ffd7400
rahul003-hub <bsetecrahul@gmail.com>
2025-09-09 11:57:38 +0530

    Update app.js

--- node_app/app.js
+++ node_app/app.js
@@ -1,11 +1,10 @@
-from flask import Flask
+const http = require('http');
 
-app = Flask(__name__)
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Node.js inside Docker!\n');
+});
 
-@app.route('/')
-def home():
-    return "Hello from Python Flask inside Docker!"
-
-if __name__ == "__main__":
-    # 0.0.0.0 makes it accessible from outside the container
-    app.run(host="0.0.0.0", port=5000)
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
